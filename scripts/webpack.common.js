f8aa7ceb8661d78353820897c5c9f762d0a62d84
Marco <marco.giacomelli@kinship.io>
2024-11-08 09:56:38 +0100

    Removed unchanged plugins

--- scripts/webpack.common.js
+++ scripts/webpack.common.js
@@ -3,16 +3,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 const gbpWebPlugins = {
-  stitch: path.join(process.cwd(), 'src', 'javascript', 'stitch', 'index.js'),
-  customPixels: path.join(process.cwd(), 'src', 'javascript', 'customPixels', 'index.js'),
-  svgize: path.join(process.cwd(), 'src', 'javascript', 'svgize', 'index.js'),
-  exampleDummy: path.join(process.cwd(), 'src', 'javascript', 'examples', 'dummy.js'),
-  exampleSkeleton: path.join(process.cwd(), 'src', 'javascript', 'examples', 'skeleton.js'),
-  randomGeneratedBunch: path.join(process.cwd(), 'src', 'javascript', 'random-generated-bunch', 'index.js'),
-  gbdkTiles: path.join(process.cwd(), 'src', 'javascript', 'gbdk-tiles', 'index.js'),
-  toSav: path.join(process.cwd(), 'src', 'javascript', 'to-sav', 'index.js'),
-  average: path.join(process.cwd(), 'src', 'javascript', 'average', 'index.js'),
-  gcode: path.join(process.cwd(), 'src', 'javascript', 'gcode', 'index.js'),
+  // stitch: path.join(process.cwd(), 'src', 'javascript', 'stitch', 'index.js'),
+  // customPixels: path.join(process.cwd(), 'src', 'javascript', 'customPixels', 'index.js'),
+  // svgize: path.join(process.cwd(), 'src', 'javascript', 'svgize', 'index.js'),
+  // exampleDummy: path.join(process.cwd(), 'src', 'javascript', 'examples', 'dummy.js'),
+  // exampleSkeleton: path.join(process.cwd(), 'src', 'javascript', 'examples', 'skeleton.js'),
+  // randomGeneratedBunch: path.join(process.cwd(), 'src', 'javascript', 'random-generated-bunch', 'index.js'),
+  // gbdkTiles: path.join(process.cwd(), 'src', 'javascript', 'gbdk-tiles', 'index.js'),
+  // toSav: path.join(process.cwd(), 'src', 'javascript', 'to-sav', 'index.js'),
+  // average: path.join(process.cwd(), 'src', 'javascript', 'average', 'index.js'),
+  // gcode: path.join(process.cwd(), 'src', 'javascript', 'gcode', 'index.js'),
   rgbandaverage: path.join(process.cwd(), 'src', 'javascript', 'rgbandaverage', 'index.js'),
 };
 
